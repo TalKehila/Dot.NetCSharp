58942f611dadfe275a6a1dd269fecb578db73db7
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 17:21:39 +0300

    How to Searialition WPF & json

---
+++ Disractor&suppfilnztion/ConsoleSerializaton/DAL/RepositoryJson.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace ConsoleSerializaton.DAL
+{
+    public class RepositoryJson
+    {
+        public void Save<T>(T data)
+        {
+            var json = new JsonSerializer { Formatting = Formatting.Indented };
+            using (var sw = new StreamWriter("data.json"))
+                json.Serialize(sw, data);
+        }
+
+        public T Load<T>()
+        {
+            var res = File.ReadAllText("data.json");
+            return JsonConvert.DeserializeObject<T>(res);
+        }
+    }
+}
