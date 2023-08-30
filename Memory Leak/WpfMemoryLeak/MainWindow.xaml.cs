838549cc7bcae5c3357eccbe6489e6471a7a9a22
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 19:09:55 +0300

    Memory leak making a loop of 1 million

---
+++ Memory Leak/WpfMemoryLeak/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfMemoryLeak
+{
+    public partial class MainWindow : Window
+    {
+        OtherWindow win;
+        public MainWindow() => InitializeComponent();
+
+        private void NewWinClick(object sender, RoutedEventArgs e)
+        {
+            if (win != null)
+            {
+                win = new OtherWindow(); //countRef++
+            }
+            win.Show();
+            win.Hide();
+
+            //var tmp = win;              //countRef++
+            win = null;                 //countRef--
+
+            GC.Collect();
+        }
+    }
+}
---
+++ Memory Leak/WpfMemoryLeak/OtherWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfMemoryLeak
+{
+    public partial class OtherWindow : Window
+    {
+        public OtherWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 1e6; i++)
+            {
+                var btn = new Button { Content = i.ToString() };
+                lvButtons.Items.Add(btn);
+            }
+        }
+    }
+}
